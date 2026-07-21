3026e8087501d4fe9b5e391087ba25504459ee8a
wairaguelijah42-star <wairaguelijah42@gmail.com>
2026-07-21 05:47:09 -0400

    Initial vulnerable application

---
+++ config.py
@@ -0,0 +1,8 @@
+DEBUG = True
+
+SECRET_KEY = "csb-jwt-2019"
+STRIPE_LIKE_GATEWAY_KEY = "pk_live_51H8x2KJ9dF7a2M4nQ7ssampleXYZ"
+
+SQLALCHEMY_DATABASE_URI = (
+    "postgresql://csb_admin:Summer2023!@10.20.4.15:5432/csb_core"
+)
